53023ff8b06fceb67c8826cfce71eec9e854c775
Stradex <stradexengine@gmail.com>
2023-10-17 16:05:03 -0300

    Test deploy

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@
 const nextConfig = {
     output: 'standalone',
      trailingSlash: true,
+    //Dummy to test deploy
     //Básicamente este images : { unoptimized: true } fue el que me permitio generar los HTML
     images: {
         unoptimized: true,
